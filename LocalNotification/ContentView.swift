ace5b957f0c1b8b4758d35bfe15e18864af00766
Benji Loya <benjiloya.apple@gmail.com>
2024-04-05 17:59:02 +0500

    Initial Commit

---
+++ LocalNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalNotification
+//
+//  Created by Benji Loya on 05.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by Benji Loya on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
